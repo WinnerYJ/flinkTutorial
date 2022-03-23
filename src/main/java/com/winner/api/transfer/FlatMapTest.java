02e49d8c52d996819582feeb98f0c292f2579c04
WinnerYJ <winneryj80@hotmail.com>
2022-03-23 11:22:40 +0800

    sink

--- src/main/java/com/winner/api/transfer/FlatMapTest.java
+++ src/main/java/com/winner/api/transfer/FlatMapTest.java
@@ -8,7 +8,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
 public class FlatMapTest {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
         executionEnvironment.setParallelism(1);
 
@@ -25,18 +25,19 @@ public class FlatMapTest {
         dataStream.flatMap((Event event, Collector<String> collector) -> {
             if (event.user.equals("Mary")) {
                 collector.collect(event.url);
-            }else{
+            } else {
                 collector.collect(event.user);
                 collector.collect(event.url);
                 collector.collect(event.timestamp.toString());
             }
-        }).returns(new TypeHint<String>() {}).print("lambda");
+        }).returns(new TypeHint<String>() {
+        }).print("lambda");
 
 
         executionEnvironment.execute();
     }
 
-    public static class MapFlatMap implements FlatMapFunction<Event, String>{
+    public static class MapFlatMap implements FlatMapFunction<Event, String> {
 
         @Override
         public void flatMap(Event event, Collector<String> collector) throws Exception {
