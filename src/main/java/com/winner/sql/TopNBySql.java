99d41631680ccadb67ce2e0f70ce9fd621abeee6
WinnerYJ <winneryj80@hotmail.com>
2022-04-06 21:56:51 +0800

    state

---
+++ src/main/java/com/winner/sql/TopNBySql.java
@@ -0,0 +1,12 @@
+package com.winner.sql;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class TopNBySql {
+    public static void main(String[] args) throws Exception{
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        environment.setParallelism(1);
+
+        StreamTableEnvironmen
+    }
+}
